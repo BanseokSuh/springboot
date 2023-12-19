0f429f5d663b4a1bca62fdaed9c13d884b16e5b5
BanseokSuh <still3028@gmail.com>
2023-12-20 00:33:46 +0900

    Initial commit, test code

---
+++ src/test/java/com/banny/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banny.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
